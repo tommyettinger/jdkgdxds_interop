465d5fa7eea3c730c1df98b36d88289c3b2fc56e
Tommy Ettinger <tommy.ettinger@gmail.com>
2021-01-09 20:32:15 -0800

    Baby steps. ObjectOrderedSet.

--- src/main/java/com/github/tommyettinger/ds/interop/JsonSupport.java
+++ src/main/java/com/github/tommyettinger/ds/interop/JsonSupport.java
@@ -2,6 +2,7 @@ package com.github.tommyettinger.ds.interop;
 
 import com.badlogic.gdx.utils.Json;
 import com.badlogic.gdx.utils.JsonValue;
+import com.github.tommyettinger.ds.ObjectOrderedSet;
 import com.github.tommyettinger.ds.ObjectSet;
 
 import javax.annotation.Nonnull;
@@ -35,6 +36,31 @@ public class JsonSupport {
             public ObjectSet read(Json json, JsonValue jsonData, Class type) {
                 if(jsonData == null || jsonData.isNull()) return null;
                 ObjectSet data = new ObjectSet<>(jsonData.size);
+                for (JsonValue value = jsonData.child; value != null; value = value.next) {
+                    data.add(json.readValue(null, value));
+                }
+                return data;
+            }
+        });
+        json.setSerializer(ObjectOrderedSet.class, new Json.Serializer<ObjectOrderedSet>() {
+            @Override
+            public void write(Json json, ObjectOrderedSet object, Class knownType) {
+                if(object == null)
+                {
+                    json.writeValue(null);
+                    return;
+                }
+                json.writeArrayStart();
+                for (Object o : object) {
+                    json.writeValue(o);
+                }
+                json.writeArrayEnd();
+            }
+
+            @Override
+            public ObjectOrderedSet read(Json json, JsonValue jsonData, Class type) {
+                if(jsonData == null || jsonData.isNull()) return null;
+                ObjectOrderedSet data = new ObjectOrderedSet<>(jsonData.size);
                 for (JsonValue value = jsonData.child; value != null; value = value.next) {
                     data.add(json.readValue(null, value));
                 }
--- src/test/java/com/github/tommyettinger/ds/interop/test/JsonTest.java
+++ src/test/java/com/github/tommyettinger/ds/interop/test/JsonTest.java
@@ -1,7 +1,9 @@
 package com.github.tommyettinger.ds.interop.test;
 
+import com.badlogic.gdx.math.GridPoint2;
 import com.badlogic.gdx.utils.Json;
 import com.badlogic.gdx.utils.JsonWriter;
+import com.github.tommyettinger.ds.ObjectOrderedSet;
 import com.github.tommyettinger.ds.ObjectSet;
 import com.github.tommyettinger.ds.interop.JsonSupport;
 import org.junit.Test;
@@ -17,6 +19,35 @@ public class JsonTest {
         ObjectSet words2 = json.fromJson(ObjectSet.class, data);
         for(Object word : words2) {
             System.out.print(word);
+            System.out.print(", ");
+        }
+        ObjectSet<GridPoint2> points = ObjectSet.with(new GridPoint2(42, 42), new GridPoint2(666, 666), new GridPoint2(23, 23));
+        data = json.toJson(points);
+        System.out.println(data);
+        ObjectSet points2 = json.fromJson(ObjectSet.class, data);
+        for(Object point : points2) {
+            System.out.print(point);
+            System.out.print(", ");
+        }
+    }
+    @Test
+    public void testObjectOrderedSet() {
+        Json json = new Json(JsonWriter.OutputType.minimal);
+        JsonSupport.registerWith(json);
+        ObjectOrderedSet<String> words = ObjectOrderedSet.with("Peanut", "Butter", "Jelly", "Time");
+        String data = json.toJson(words);
+        System.out.println(data);
+        ObjectOrderedSet words2 = json.fromJson(ObjectOrderedSet.class, data);
+        for(Object word : words2) {
+            System.out.print(word);
+            System.out.print(", ");
+        }
+        ObjectOrderedSet<GridPoint2> points = ObjectOrderedSet.with(new GridPoint2(42, 42), new GridPoint2(666, 666), new GridPoint2(23, 23));
+        data = json.toJson(points);
+        System.out.println(data);
+        ObjectOrderedSet points2 = json.fromJson(ObjectOrderedSet.class, data);
+        for(Object point : points2) {
+            System.out.print(point);
             System.out.print(", ");
         }
     }
