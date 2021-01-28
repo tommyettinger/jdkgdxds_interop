3feabdd4f6075717ff8b4cedc9fbd0898717c853
Tommy Ettinger <tommy.ettinger@gmail.com>
2021-01-27 21:36:35 -0800

    Conversions to libGDX primitive Arrays

--- src/main/java/com/github/tommyettinger/ds/interop/Conversion.java
+++ src/main/java/com/github/tommyettinger/ds/interop/Conversion.java
@@ -1,13 +1,12 @@
 package com.github.tommyettinger.ds.interop;
 
 import com.badlogic.gdx.utils.*;
-import com.github.tommyettinger.ds.FloatList;
-import com.github.tommyettinger.ds.IntList;
-import com.github.tommyettinger.ds.LongList;
-import com.github.tommyettinger.ds.ObjectList;
+import com.github.tommyettinger.ds.*;
+import com.github.tommyettinger.ds.support.util.FloatIterator;
 
 import java.util.Collection;
 import java.util.Map;
+import java.util.PrimitiveIterator;
 
 public class Conversion {
 
@@ -49,6 +48,21 @@ public class Conversion {
     }
 
     /**
+     * Can be used to convert from any jdkgdxds int-based collection (such as {@link IntList},
+     * {@link com.github.tommyettinger.ds.IntSet}, or {@link IntObjectMap.Keys}) to a libGDX IntArray.
+     * @param from a primitive-int-backed data structure from jdkgdxds
+     * @return a new IntArray holding the items of {@code from}
+     */
+    public static IntArray toIntArray(PrimitiveCollection.OfInt from) {
+        IntArray array = new IntArray(from.size());
+        PrimitiveIterator.OfInt it = from.iterator();
+        while (it.hasNext()){
+            array.add(it.nextInt());
+        }
+        return array;
+    }
+
+    /**
      * Can be used to convert from a libGDX LongArray to a jdkgdxds LongList.
      * @param from a libGDX LongArray
      * @return a new LongList holding the items of {@code from}
@@ -58,12 +72,42 @@ public class Conversion {
     }
 
     /**
+     * Can be used to convert from any jdkgdxds long-based collection (such as {@link LongList},
+     * {@link com.github.tommyettinger.ds.LongSet}, or {@link LongObjectMap.Keys}) to a libGDX LongArray.
+     * @param from a primitive-long-backed data structure from jdkgdxds
+     * @return a new LongArray holding the items of {@code from}
+     */
+    public static LongArray toLongArray(PrimitiveCollection.OfLong from) {
+        LongArray array = new LongArray(from.size());
+        PrimitiveIterator.OfLong it = from.iterator();
+        while (it.hasNext()){
+            array.add(it.nextLong());
+        }
+        return array;
+    }
+
+    /**
      * Can be used to convert from a libGDX FloatArray to a jdkgdxds FloatList.
      * @param from a libGDX FloatArray
      * @return a new FloatList holding the items of {@code from}
      */
     public static FloatList toFloatList(FloatArray from){
         return new FloatList(from.items, 0, from.size);
+    }
+
+    /**
+     * Can be used to convert from any jdkgdxds float-based collection (such as {@link FloatList}
+     * or {@link com.github.tommyettinger.ds.ObjectFloatMap.Values}) to a libGDX FloatArray.
+     * @param from a primitive-float-backed data structure from jdkgdxds
+     * @return a new FloatArray holding the items of {@code from}
+     */
+    public static FloatArray toFloatArray(PrimitiveCollection.OfFloat from) {
+        FloatArray array = new FloatArray(from.size());
+        FloatIterator it = from.iterator();
+        while (it.hasNext()){
+            array.add(it.nextFloat());
+        }
+        return array;
     }
 
     /**
