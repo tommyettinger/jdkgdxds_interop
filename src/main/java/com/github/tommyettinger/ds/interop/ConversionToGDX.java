714f89c68aa6a2566ef68eea84e5cf1086056c2a
Tommy Ettinger <tommy.ettinger@gmail.com>
2021-03-20 22:00:11 -0700

    Update jdkgdxds, improve generics in ConversionToGDX.

--- src/main/java/com/github/tommyettinger/ds/interop/ConversionToGDX.java
+++ src/main/java/com/github/tommyettinger/ds/interop/ConversionToGDX.java
@@ -31,7 +31,7 @@ public class ConversionToGDX {
      * @param <T> the element type for {@code from} and the result
      * @return a new Array of type T holding the items of {@code from} (this does not give a Class to the Array constructor)
      */
-    public static <T> Array<T> toArray(Collection<T> from) {
+    public static <T> Array<T> toArray(Collection<? extends T> from) {
         Array<T> array = new Array<>(from.size());
         for(T t : from)
             array.add(t);
@@ -107,9 +107,9 @@ public class ConversionToGDX {
      * @param <V> the value type
      * @return a new ArrayMap holding the keys and values of {@code from} (this does not give a Class to the ArrayMap constructor)
      */
-    public static <K, V> ArrayMap<K, V> toArrayMap(Map<K, V> from) {
+    public static <K, V> ArrayMap<K, V> toArrayMap(Map<? extends K, ? extends V> from) {
         ArrayMap<K, V> arrayMap = new ArrayMap<>(true, from.size());
-        for(Map.Entry<K, V> e : from.entrySet())
+        for(Map.Entry<? extends K, ? extends V> e : from.entrySet())
             arrayMap.put(e.getKey(), e.getValue());
         return arrayMap;
     }
@@ -120,9 +120,9 @@ public class ConversionToGDX {
      * @param from anything that implements the JDK Collection interface
      * @return a new libGDX ObjectSet holding the unique items in {@code from}
      */
-    public static <T> ObjectSet<T> toObjectSet(Collection<T> from) {
+    public static <T> ObjectSet<T> toObjectSet(Collection<? extends T> from) {
         ObjectSet<T> set = new ObjectSet<>(from.size());
-        Iterator<T> it = from.iterator();
+        Iterator<? extends T> it = from.iterator();
         while (it.hasNext()) {
             set.add(it.next());
         }
@@ -135,9 +135,9 @@ public class ConversionToGDX {
      * @param from anything that implements the JDK Collection interface
      * @return a new libGDX OrderedSet holding the unique items in {@code from}
      */
-    public static <T> OrderedSet<T> toOrderedSet(Collection<T> from) {
+    public static <T> OrderedSet<T> toOrderedSet(Collection<? extends T> from) {
         OrderedSet<T> set = new OrderedSet<>(from.size());
-        Iterator<T> it = from.iterator();
+        Iterator<? extends T> it = from.iterator();
         while (it.hasNext()) {
             set.add(it.next());
         }
@@ -168,7 +168,7 @@ public class ConversionToGDX {
      * @param <V> the type of values; the same in {@code from} and the returned ObjectMap
      * @return a new ObjectMap holding all of the key-value pairs in {@code from}
      */
-    public static <K, V> ObjectMap<K, V> toObjectMap(Map<K, V> from){
+    public static <K, V> ObjectMap<K, V> toObjectMap(Map<? extends K, ? extends V> from){
         ObjectMap<K, V> map = new ObjectMap<>(from.size());
         for(K k : from.keySet()) {
             map.put(k, from.get(k));
@@ -186,7 +186,7 @@ public class ConversionToGDX {
      * @param <V> the type of values; the same in {@code from} and the returned OrderedMap
      * @return a new OrderedMap holding all of the key-value pairs in {@code from}
      */
-    public static <K, V> OrderedMap<K, V> toOrderedMap(Map<K, V> from){
+    public static <K, V> OrderedMap<K, V> toOrderedMap(Map<? extends K, ? extends V> from){
         OrderedMap<K, V> map = new OrderedMap<>(from.size());
         for(K k : from.keySet()) {
             map.put(k, from.get(k));
@@ -201,7 +201,7 @@ public class ConversionToGDX {
      * @param <K> the type of keys; the same in {@code from} and the returned ObjectIntMap
      * @return a new libGDX ObjectIntMap holding all of the key-value pairs in {@code from}
      */
-    public static <K> ObjectIntMap<K> toObjectIntMap(com.github.tommyettinger.ds.ObjectIntMap<K> from){
+    public static <K> ObjectIntMap<K> toObjectIntMap(com.github.tommyettinger.ds.ObjectIntMap<? extends K> from){
         ObjectIntMap<K> map = new ObjectIntMap<>(from.size());
         for(K k : from.keySet()) {
             map.put(k, from.get(k));
@@ -216,7 +216,7 @@ public class ConversionToGDX {
      * @param <K> the type of keys; the same in {@code from} and the returned ObjectLongMap
      * @return a new libGDX ObjectLongMap holding all of the key-value pairs in {@code from}
      */
-    public static <K> ObjectLongMap<K> toObjectLongMap(com.github.tommyettinger.ds.ObjectLongMap<K> from){
+    public static <K> ObjectLongMap<K> toObjectLongMap(com.github.tommyettinger.ds.ObjectLongMap<? extends K> from){
         ObjectLongMap<K> map = new ObjectLongMap<>(from.size());
         for(K k : from.keySet()) {
             map.put(k, from.get(k));
@@ -231,7 +231,7 @@ public class ConversionToGDX {
      * @param <K> the type of keys; the same in {@code from} and the returned ObjectFloatMap
      * @return a new libGDX ObjectFloatMap holding all of the key-value pairs in {@code from}
      */
-    public static <K> ObjectFloatMap<K> toObjectFloatMap(com.github.tommyettinger.ds.ObjectFloatMap<K> from){
+    public static <K> ObjectFloatMap<K> toObjectFloatMap(com.github.tommyettinger.ds.ObjectFloatMap<? extends K> from){
         ObjectFloatMap<K> map = new ObjectFloatMap<>(from.size());
         for(K k : from.keySet()) {
             map.put(k, from.get(k));
@@ -246,7 +246,7 @@ public class ConversionToGDX {
      * @param <V> the type of values; the same in {@code from} and the returned IntMap
      * @return a new libGDX IntMap holding all of the key-value pairs in {@code from}
      */
-    public static <V> IntMap<V> toIntMap(com.github.tommyettinger.ds.IntObjectMap<V> from){
+    public static <V> IntMap<V> toIntMap(com.github.tommyettinger.ds.IntObjectMap<? extends V> from){
         IntMap<V> map = new IntMap<>(from.size());
         PrimitiveIterator.OfInt it = from.keySet().iterator();
         while (it.hasNext()) {
@@ -295,7 +295,7 @@ public class ConversionToGDX {
      * @param <V> the type of values; the same in {@code from} and the returned IntMap
      * @return a new libGDX IntMap holding all of the key-value pairs in {@code from}
      */
-    public static <V> LongMap<V> toLongMap(com.github.tommyettinger.ds.LongObjectMap<V> from){
+    public static <V> LongMap<V> toLongMap(com.github.tommyettinger.ds.LongObjectMap<? extends V> from){
         LongMap<V> map = new LongMap<>(from.size());
         PrimitiveIterator.OfLong it = from.keySet().iterator();
         while (it.hasNext()) {
@@ -306,7 +306,7 @@ public class ConversionToGDX {
     }
 
     /**
-     * Can be used to convert from any JDK Map, such as a {@link java.util.HashMap}, {@link ObjectObjectMap},
+     * Can be used to convert from any JDK Map, such as a {@link java.util.HashMap}, {@link IdentityObjectMap},
      * or {@link CaseInsensitiveOrderedMap}, to a libGDX IdentityMap. In almost all cases, IdentityMap is more
      * permissive about allowing duplicate keys, so converting to an IdentityMap should be lossless.
      * @param from any Map that has a usable {@link Map#keySet()} method
@@ -314,7 +314,7 @@ public class ConversionToGDX {
      * @param <V> the type of values; the same in {@code from} and the returned IdentityMap
      * @return a new IdentityMap holding all of the key-value pairs in {@code from}
      */
-    public static <K, V> IdentityMap<K, V> toIdentityMap(Map<K, V> from){
+    public static <K, V> IdentityMap<K, V> toIdentityMap(Map<? extends K, ? extends V> from){
         IdentityMap<K, V> map = new IdentityMap<>(from.size());
         for(K k : from.keySet()) {
             map.put(k, from.get(k));
